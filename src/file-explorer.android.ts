257da039562f978fe541b3835878dab466397ff4
BalaDhruv <androbalamail@gmail.com>
2018-10-30 18:24:05 +0530

    next

--- src/file-explorer.android.ts
+++ src/file-explorer.android.ts
@@ -1,6 +1,16 @@
 import { FileExplorerBase } from './file-explorer.common';
 
+declare const android: any;
+
 export class FileExplorer extends FileExplorerBase {
+    androidLib: any;
+    storageList: any;
+
+    constructor() {
+        super();
+        this.androidLib = android.support.v4.content.ContextCompat;
+    }
+
     public listStorage(): void {
         console.log('Listing Storages..');
     }
